ac5a73c7230941b3b06aa35bc83a9c923ee5cd54
Werostr <werost@student.agh.edu.pl>
2023-08-25 14:38:51 +0200

    test commit

--- seeds/index.js
+++ seeds/index.js
@@ -1,29 +1,28 @@
-const mongoose = require('mongoose');
-const Fungus = require('../models/fungus');
+const mongoose = require("mongoose");
+const Fungus = require("../models/fungus");
 const varieties = require("./varieties");
 
-mongoose.connect('mongodb://127.0.0.1:27017/fungi-app');
+mongoose.connect("mongodb://127.0.0.1:27017/fungi-app");
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error:"));
-db.once('open', () => {
-    console.log("DATABASE CONNECTED");
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("DATABASE CONNECTED");
 });
 
 const seedDB = async () => {
-    await Fungus.deleteMany({});
-    for (let i = 0; i < 5; i++) {
-        const f = new Fungus({
-            variety: `${varieties[i].variety}`,
-            poisonous: `${varieties[i].poisonous}`,
-            description: `${varieties[i].description}`,
-            city: `${varieties[i].city}`,
-            country: `${varieties[i].country}`,
-            image: `${varieties[i].image}`
-        });
-        await f.save();
-    }
-
+  await Fungus.deleteMany({});
+  for (let i = 0; i < 5; i++) {
+    const f = new Fungus({
+      variety: `${varieties[i].variety}`,
+      poisonous: `${varieties[i].poisonous}`,
+      description: `${varieties[i].description}`,
+      city: `${varieties[i].city}`,
+      country: `${varieties[i].country}`,
+      image: `${varieties[i].image}`,
+    });
+    await f.save();
+  }
 };
 
 seedDB();
